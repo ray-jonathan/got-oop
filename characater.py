58544dfd9b5f15069bb8b372a89e833eacd53418
Jonathan Ray <jonathanray17@gmail.com>
2019-02-20 11:22:16 -0500

    adds back the file that I tried to remove

---
+++ characater.py
@@ -0,0 +1,12 @@
+# name
+# avatar
+# inventory
+
+# class names a single words, with the first letter capitalized
+class Character():
+    # the "dunder init" method is the constructor
+    def __init__(self, new_name, new_avatar):
+        # `self` is the customary way to refer to the instance being built
+        # in some other languages, they use `this` instead
+        self.name = new_name
+        self.avatar = new_avatar
