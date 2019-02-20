b3a9e759346d410e63c7db5c336cf830796d4e37
Jonathan Ray <jonathanray17@gmail.com>
2019-02-20 10:16:32 -0500

    updates constructor to set the name attribute

--- characater.py
+++ characater.py
@@ -4,4 +4,8 @@
 
 # class names a single words, with the first letter capitalized
 class Character():
-    pass
+    # the "dunder init" method is the constructor
+    def __init__(self, new_name):
+        # `self` is the customary way to refer to the instance being built
+        # in some other languages, they use `this` instead
+        self.name = new_name
